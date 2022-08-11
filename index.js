876af4644d134b3e9a910684ab0c17ded5516a7e
Mario Blazquez <mario.blazquez.m@gmail.com>
2022-08-11 18:15:31 +0200

    format documents

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 window.onload = () => {
-    const arrow = document.querySelector("#arrow");
-    const section = document.querySelector(".projectDetailed")
-    section.setAttribute("class","projectDetailed section")
-    arrow.removeAttribute("class")
-    
-    function changeSize() {
-        if(section.getAttribute("class") == "projectDetailed projectOpened section"){
-            section.setAttribute("class","projectDetailed section")
-            arrow.removeAttribute("class")
-        } else {
-            section.setAttribute("class","projectDetailed projectOpened section")
-            arrow.setAttribute("class", "arrowDown")
-        }
-    }
-    arrow.addEventListener("click", changeSize)
-}
+  const arrow = document.querySelector("#arrow");
+  const section = document.querySelector(".projectDetailed");
+  section.setAttribute("class", "projectDetailed section");
+  arrow.removeAttribute("class");
 
+  function changeSize() {
+    if (
+      section.getAttribute("class") == "projectDetailed projectOpened section"
+    ) {
+      section.setAttribute("class", "projectDetailed section");
+      arrow.removeAttribute("class");
+    } else {
+      section.setAttribute("class", "projectDetailed projectOpened section");
+      arrow.setAttribute("class", "arrowDown");
+    }
+  }
+  arrow.addEventListener("click", changeSize);
+};
